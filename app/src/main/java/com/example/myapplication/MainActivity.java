984a0a6bbed1b7176388bb914ed9ecaa6cdda833
Anthony <arteaga17000@gmail.com>
2022-08-26 14:02:45 -0500

    v0.1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
