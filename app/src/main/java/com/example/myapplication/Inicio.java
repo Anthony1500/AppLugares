26ff41c9852223c82185713cae5eab27ca52f035
dodybryan <johnparedes209@gmail.com>
2022-08-30 12:55:16 -0500

    pantallas

--- app/src/main/java/com/example/myapplication/Inicio.java
+++ app/src/main/java/com/example/myapplication/Inicio.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Inicio extends AppCompatActivity {
-
+    Button boton_tiket,boton_lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        boton_tiket=(Button)findViewById(R.id.btn_tiket);
+        boton_lista=(Button)findViewById(R.id.btn_lista);
+        boton_tiket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), tiket.class);
+                startActivity(intent);
+
+            }
+        });
+        boton_lista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), lista.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
